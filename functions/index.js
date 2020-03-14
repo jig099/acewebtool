8b9a5d4f34a50e1337b38e7e50a0b93137f36615
Jiaxin Ge <jig099@ucsd.edu>
2020-03-13 17:48:57 -0700

    done

--- functions/index.js
+++ functions/index.js
@@ -320,6 +320,7 @@ exports.setOwner = functions.https.onRequest((req,res)=>{
      .catch(e=>console.log(e))
   }
 });
+
 exports.addAdmin = functions.https.onRequest((req,res)=>{
   res.set("Access-Control-Allow-Origin", "https://acewebtool.firebaseapp.com");
   res.set("Access-Control-Allow-Credentials", "true");
@@ -372,17 +373,58 @@ exports.addAdmin = functions.https.onRequest((req,res)=>{
 
 })
 
-//addUser 
+exports.deleteAccount = functions.https.onRequest((req,res)=>{
+  res.set("Access-Control-Allow-Origin", "https://acewebtool.firebaseapp.com");
+  res.set("Access-Control-Allow-Credentials", "true");
 
-//send back uid 
-/*exports.setAdmin = functions.https.onCall((data,context) =>{
-  //first we need to check if this person has the right to setAdmin
-  if(!context.isOwner){
-    return{status: 'error', message: 'Not admin'}
-  }else{
-    
+  if (req.method === "OPTIONS") {
+    // Send response to OPTIONS requests
+    res.set("Access-Control-Allow-Methods", "POST");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Max-Age", "3600");
+    res.status(204).send("");
+  } else {
+
+    let body = JSON.parse(req.body)
+     
+    // parse data
+    let otherUID = body.otherUID;
+    let currUID = body.currUID;
+
+    // check if user is owner
+    let ownerUID = 'UEFMvCcQ9Wd0n3E2hxDuI0LYxqu1'
+
+    //if the user is owner
+    if(currUID == ownerUID){
+      admin.auth().deleteUser(otherUID)
+      .then(function() {
+        res.status(200).send('Successfully deleted user');
+        console.log('Successfully deleted user');
+      })
+      .catch(function(error) {
+        console.log('Error deleting user:', error);
+      });
+      //check if the user is admin
+    }
+    else{
+      admin.auth().verifyIdToken(idToken).then((claims) => {
+        if (claims.admin === true) {
+          admin.auth().deleteUser(otherUID)
+          .then(function() {
+            console.log('Successfully deleted user');
+            res.status(200).send('Successfully deleted user');
+          })
+          .catch(function(error) {
+            console.log('Error deleting user:', error);
+          });
+        }
+        else{
+          res.status(405).send("you have no rights to delete")
+        }
+      });
+    }
   }
-});*/
+});
 
 
 //getting all admin info from the db
--- public/scripts/analytics.js
+++ public/scripts/analytics.js
@@ -222,24 +222,6 @@ function drawTable(d) {
 }
 
 
-// set up owner priv 
-
-// let ownerUID = 'UEFMvCcQ9Wd0n3E2hxDuI0LYxqu1'
-// let data = {'uid':ownerUID}
-//   fetch(
-//     "https://us-central1-acewebtool.cloudfunctions.net/setOwner",
-//     sendbody('POST', data)
-//   )
-//     .then(response => console.log(response));
-
-
-
-
-// addAdmin 
-
-// TODO DEBUG 
-
-
 let adminEmail = 'yu1234@ucsd.edu'
 let ownerUID = 'UEFMvCcQ9Wd0n3E2hxDuI0LYxqu1'
 let adminPassword = '1234567'
@@ -274,9 +256,30 @@ function editAccount(currUID, newEmail, newPwd, endPoint){
       console.log(data)
       resolve(data);
     })
-  .catch(reject(Error("Error"))
+  .catch(reject(Error("Error")))
 
   })
 
+}
+
+function deleteAccount(currUID,otherUID,endPoint){
+  return new Promise((resolve, reject) => {
+
+    let data = {'currUID':currUID, 'otherUID':otherUID};
+    let endPointUrl = "https://us-central1-acewebtool.cloudfunctions.net/";
+  
+    endPointUrl += endPoint;
+     fetch(
+      endPointUrl, 
+      sendbody('POST', data)
+    )
+    .then(r => r.json())
+    .then(data => 
+      {
+        console.log(data)
+      })
+    .catch(reject(Error("Error")))
+  
+    })
 }
 
